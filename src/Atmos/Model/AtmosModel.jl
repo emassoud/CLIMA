8d208afa517b5eb29ad802e3209eab398ef6d9a1
Brandon Allen <ballen@mit.edu>
2020-01-10 14:41:50 -0500

    Fix atmosphere side

--- src/Atmos/Model/AtmosModel.jl
+++ src/Atmos/Model/AtmosModel.jl
@@ -147,7 +147,7 @@ end
                                  diffusive::Vars, aux::Vars, t::Real)
   ρinv = 1/state.ρ
   u = ρinv * state.ρu
-  
+
   # diffusive
   ρτ = diffusive.ρτ
   ρd_h_tot = diffusive.ρd_h_tot
@@ -213,7 +213,7 @@ function update_aux!(dg::DGModel, m::AtmosModel, Q::MPIStateArray, t::Real)
 end
 
 function atmos_nodal_update_aux!(m::AtmosModel, state::Vars, aux::Vars,
-                                 diff::Vars, t::Real)
+                                 t::Real)
   atmos_nodal_update_aux!(m.moisture, m, state, aux, t)
   atmos_nodal_update_aux!(m.radiation, m, state, aux, t)
   atmos_nodal_update_aux!(m.turbulence, m, state, aux, t)
